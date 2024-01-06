815e74fa0fca2c12aabe647842bf316d0e3ad60e
Your Name <m.karaarslan06@outlook.com>
2024-01-06 16:53:00 +0300

    Project-initilazed

---
+++ screens/CategoriesScreen.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { CATEGORIES } from '../data/dummy-data'
+
+export default function CategoriesScreen() {
+  return (
+    <View>
+      <Text>CategoriesScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
