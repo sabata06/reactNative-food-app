cbb191179b643b95729361680f1a60cc7965ee8b
Your Name <m.karaarslan06@outlook.com>
2024-01-11 23:03:09 +0300

    favoritesscreen updated via redux and project finished

--- screens/FavoritesScreen.jsx
+++ screens/FavoritesScreen.jsx
@@ -3,33 +3,38 @@ import React, { useContext } from "react";
 import { FavoritesContext } from "../store/favoritesContext";
 import { FOODS } from "../data/dummy-data";
 import FoodList from "../components/FoodList";
+import { useSelector, useDispatch } from "react-redux";
 
 export default function FavoritesScreen() {
-  const favoriteFoodContext = useContext(FavoritesContext);
+  // const favoriteFoodContext = useContext(FavoritesContext);
+  const favoriteFoodsIds = useSelector((state) => state.favoriteFoods.ids);
 
+  // const favoriteFoods = FOODS.filter((food) =>
+  //   favoriteFoodContext.ids.includes(food.id)
+  // );
   const favoriteFoods = FOODS.filter((food) =>
-    favoriteFoodContext.ids.includes(food.id)
+    favoriteFoodsIds.includes(food.id)
   );
 
-if(favoriteFoods.length === 0){
-  return(
-    <View style={styles.container} >
-      <Text  style={styles.text}>You Have No Favorite Foods</Text>
-    </View>
-  )
-}
+  if (favoriteFoods.length === 0) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>You Have No Favorite Foods</Text>
+      </View>
+    );
+  }
 
   return <FoodList items={favoriteFoods} />;
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    alignItems:"center",
-    justifyContent:"center"
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
-  text:{
-    fontSize:18,
-    fontWeight:"bold"
-  }
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
 });
